953b4fb16ff212641650ce52d0a1a59c8a6c07f9
babuush <thanapatpiyamassakul@gmail.com>
2024-04-28 00:34:10 +0700

    Updated course.py

--- course.py
+++ course.py
@@ -19,6 +19,6 @@ with open('course.json') as f:
         language_type.append((course["program_type_name_th"]))
 
 uni = pd.DataFrame([university_id, university_name, campus, faculty, program, language_type]).transpose()
-uni.columns = ['UNI_ID', 'UNI_NAME'], 'CAMPUS, FACULTY, PROGRAM, LANGUAGE_TYPE'
+uni.columns = ['UNI_ID', 'UNI_NAME', 'CAMPUS, FACULTY, PROGRAM, LANGUAGE_TYPE']
 uni.dropna(inplace=True)
 uni.to_csv(r'/Users/thanapatpiyamssakul/coding/first_year/de141/course.csv', index=False)
