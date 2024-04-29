75e7714980d50b453475334871b96d46ab27e159
babuush <thanapatpiyamassakul@gmail.com>
2024-04-29 22:47:44 +0700

    Updated course.py

--- course.py
+++ course.py
@@ -1,26 +1,22 @@
 import json
 import pandas as pd
 
-university_id = []
-university_name = []
-campus = []
-faculty = []
-dept = []
-program = []
-language_type = []
-
+# Load JSON data
 with open('course.json') as f:
     courses = json.load(f)
-    for course in courses:
-        university_id.append(course["university_id"])
-        university_name.append(course["university_name_en"])
-        campus.append(course["campus_name_en"])
-        faculty.append(course["faculty_name_en"])
-        dept.append(course["field_name_en"])
-        program.append((course["program_name_en"]))
-        language_type.append((course["program_type_name_th"]))
 
-uni = pd.DataFrame([university_id, university_name, campus, faculty, dept, program, language_type]).transpose()
+# Create DataFrame from JSON data
+uni = pd.DataFrame.from_records(courses)
+
+# Select relevant columns
+uni = uni[['university_id', 'university_name_en', 'campus_name_en', 'faculty_name_en', 'field_name_en',
+           'program_name_en', 'program_type_name_th']]
+
+# Rename columns
 uni.columns = ['UNI_ID', 'UNI_NAME', 'CAMPUS', 'FACULTY', 'DEPT', 'PROGRAM', 'LANGUAGE_TYPE']
+
+# Drop rows with missing values
 uni.dropna(inplace=True)
-uni.to_csv(r'/Users/thanapatpiyamssakul/coding/first_year/de141/courses.csv', index=False)
+
+# Save DataFrame to CSV
+uni.to_csv('/Users/thanapatpiyamssakul/coding/first_year/de141/courses0.csv', index=False)
