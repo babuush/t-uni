d61f5cb91266a3b57479a86d524ded58fc5cba5c
babuush <thanapatpiyamassakul@gmail.com>
2024-04-29 22:23:12 +0700

    Updated course.py

--- course.py
+++ course.py
@@ -5,6 +5,7 @@ university_id = []
 university_name = []
 campus = []
 faculty = []
+dept = []
 program = []
 language_type = []
 
@@ -15,10 +16,11 @@ with open('course.json') as f:
         university_name.append(course["university_name_en"])
         campus.append(course["campus_name_en"])
         faculty.append(course["faculty_name_en"])
+        dept.append(course["field_name_en"])
         program.append((course["program_name_en"]))
         language_type.append((course["program_type_name_th"]))
 
-uni = pd.DataFrame([university_id, university_name, campus, faculty, program, language_type]).transpose()
-uni.columns = ['UNI_ID', 'UNI_NAME', 'CAMPUS', 'FACULTY', 'PROGRAM', 'LANGUAGE_TYPE']
+uni = pd.DataFrame([university_id, university_name, campus, faculty, dept, program, language_type]).transpose()
+uni.columns = ['UNI_ID', 'UNI_NAME', 'CAMPUS', 'FACULTY', 'DEPT', 'PROGRAM', 'LANGUAGE_TYPE']
 uni.dropna(inplace=True)
-uni.to_csv(r'/Users/thanapatpiyamssakul/coding/first_year/de141/course.csv', index=False)
+uni.to_csv(r'/Users/thanapatpiyamssakul/coding/first_year/de141/courses.csv', index=False)
